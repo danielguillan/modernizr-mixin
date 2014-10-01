503b27bff657847c42cb0c509d251a13d4ac1809
Daniel Guillan <daniel.guillan@gmail.com>
2014-10-01 17:20:58 +0200

    Namespace the private _modernizr mixin

--- lib/modernizr-mixin.rb
+++ lib/modernizr-mixin.rb
@@ -3,6 +3,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('modernizr-mixin', :path => extension_path)
 
 module ModernizrMixin
-  VERSION = "3.0.0"
-  DATE = "2014-08-31"
+  VERSION = "3.0.1"
+  DATE = "2014-10-01"
 end
